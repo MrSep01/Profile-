672fc9306086824bdf69d8a6634bcc53a5dc9ee8
Sep.alamouti <sep.alamouti@sepalamouti.com>
2026-02-18 14:07:41 +0700

    Use production Turnstile site key and refresh asset cache

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@ window.SUPABASE_CONFIG = {
   url: "https://zjqkzjepeqbjhzfytmcz.supabase.co",
   anonKey: "sb_publishable_67J_CJZqF3pasfnThD-lag_edAZhVTw",
   edgeFunctionName: "engagement",
-  turnstileSiteKey: "1x00000000000000000000AA"
+  turnstileSiteKey: "0x4AAAAAACfBB9JiRu1n35i_"
 };
