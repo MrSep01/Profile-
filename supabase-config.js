7368855197e2d2978e2a42987d36c9e325e25e28
Sep.alamouti <sep.alamouti@sepalamouti.com>
2026-02-18 13:45:44 +0700

    Enable Turnstile test keys and enforce verification in live config

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@ window.SUPABASE_CONFIG = {
   url: "https://zjqkzjepeqbjhzfytmcz.supabase.co",
   anonKey: "sb_publishable_67J_CJZqF3pasfnThD-lag_edAZhVTw",
   edgeFunctionName: "engagement",
-  turnstileSiteKey: ""
+  turnstileSiteKey: "1x00000000000000000000AA"
 };
